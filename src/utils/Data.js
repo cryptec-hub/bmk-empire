cf213fab6c4647262ed45c2cf08892d0b246fba2
Devaula <njigupaul22@gmail.com>
2023-03-15 16:41:46 +0300

    Added footer

---
+++ src/utils/Data.js
@@ -0,0 +1,21 @@
+export const services = [
+  {
+    id: 1,
+    title: "Service 1",
+    tagline: "Service 1 description",
+    image: "service1.jpg",
+  },
+
+  {
+    id: 9,
+    title: "Service 9",
+    tagline: "Service 9 description",
+    image: "service9.jpg",
+  },
+  {
+    id: 10,
+    title: "Service 10",
+    tagline: "Service 10 description",
+    image: "service10.jpg",
+  },
+];
